b3d131b94f9417c0c4cd6733433c86ca780dde5e
Adrian Kummerlaender <adrian@kummerlaender.eu>
2019-10-24 14:43:06 +0200

    Handle missing memory layout argument

--- boltzgen.py
+++ boltzgen.py
@@ -5,9 +5,14 @@ from boltzgen import *
 
 argparser = argparse.ArgumentParser(description='Generate LBM kernels in various languages using a symbolic description.')
 argparser.add_argument('language', help = 'Target language (currently either "opencl" or "cpp")')
-argparser.add_argument('layout',   help = 'Memory layout ("aos" or "soa" for C++, ignored for OpenCL')
+argparser.add_argument(
+    '--layout', dest = 'layout',
+    help = 'Memory layout ("aos" or "soa" for C++, ignored for OpenCL')
 
 args = argparser.parse_args()
+
+if args.language == 'cpp' and args.layout is None:
+    raise Exception('Please specify the memory layout')
 
 lbm = LBM(D2Q9)
 generator = Generator(
