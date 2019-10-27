6e99286c9a756272e972fe21212b094de95d36f6
Adrian Kummerlaender <adrian@kummerlaender.eu>
2019-10-27 19:18:20 +0100

    Tidy up shell environment

--- shell.nix
+++ shell.nix
@@ -5,59 +5,19 @@ pkgs.stdenvNoCC.mkDerivation rec {
   env = pkgs.buildEnv { name = name; paths = buildInputs; };
 
   buildInputs = let
-    custom-python = let
-      packageOverrides = self: super: {
-        pyopencl = super.pyopencl.overridePythonAttrs(old: rec {
-          buildInputs = with pkgs; [
-            opencl-headers ocl-icd python37Packages.pybind11
-            libGLU_combined
-          ];
-        # Enable OpenGL integration and fix build
-          preBuild = ''
-            python configure.py --cl-enable-gl
-            export HOME=/tmp/pyopencl
-          '';
-        });
-      };
-    in pkgs.python3.override { inherit packageOverrides; };
-
-    pyevtk = pkgs.python3.pkgs.buildPythonPackage rec {
-      pname = "PyEVTK";
-      version = "1.2.1";
-
-      src = pkgs.fetchFromGitHub {
-        owner  = "paulo-herrera";
-        repo   = "PyEVTK";
-        rev    = "v1.2.1";
-        sha256 = "1p2459dqvgakywvy5d31818hix4kic6ks9j4m582ypxyk5wj1ksz";
-      };
-
-      buildInputs = with pkgs.python37Packages; [
-        numpy
-      ];
-
-      doCheck = false;
-    };
-
-    local-python = custom-python.withPackages (python-packages: with python-packages; [
+    local-python = pkgs.python3.withPackages (python-packages: with python-packages; [
       numpy
       sympy
-      pyopencl
-      pyopengl
-      pyrr
       Mako
-      pyevtk
     ]);
 
   in [
     local-python
-    pkgs.opencl-info
     pkgs.universal-ctags
   ];
 
   shellHook = ''
     export NIX_SHELL_NAME="${name}"
-    export PYOPENCL_COMPILER_OUTPUT=1
     export PYTHONPATH="$PWD/boltzgen:$PYTHONPATH"
     export PYTHONDONTWRITEBYTECODE=1
   '';
