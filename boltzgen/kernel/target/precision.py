1b9ac6e7aee3cf63495a65c2d7dbf79a0be23d7d
Adrian Kummerlaender <adrian@kummerlaender.eu>
2019-10-27 22:09:27 +0100

    Verify precision parameter

--- boltzgen/kernel/target/precision.py
+++ boltzgen/kernel/target/precision.py
@@ -1,15 +1,16 @@
-class cpp:
+class common:
     @classmethod
     def get_float_type(self, precision):
+        if precision not in ['single', 'double']:
+            raise Exception("Precision must be either 'single' or 'double'")
+
         return {
             'single': 'float',
             'double': 'double'
         }.get(precision)
 
-class cl:
-    @classmethod
-    def get_float_type(self, precision):
-        return {
-            'single': 'float',
-            'double': 'double'
-        }.get(precision)
+class cpp(common):
+    pass
+
+class cl(common):
+    pass
